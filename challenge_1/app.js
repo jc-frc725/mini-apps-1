ff001d4b8ada10c6a064ab25bec5f95b4ea9cfd7
Justin Chow <hrll71990@gmail.com>
2021-01-04 15:44:31 -0800

    refactoring controller click handlers to use nested for loop

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -21,39 +21,59 @@ const boardModel = {
 }
 // initialize board?
 document.addEventListener('DOMContentLoaded', event => {
+  // verbose set up for boardModel's rows and cells
+  boardModel._rows.forEach(row => {
+    row[0] = '';
+    row[1] = '';
+    row[2] = '';
+  })
+
   // first move always starts on player X
   boardModel.placeX = true;
 
   // initialize boardModel rows, contains no X or O, blank
   for (let i = 0; i < boardModel._rows.length; i++) {
     for (let j = 0; j < boardModel._rows[i].length; j++) {
-      let blank = "&nbsp &nbsp &nbsp &nbsp &nbsp"
-      boardModel._rows[i][j] = blank;
+      // let blank = "&nbsp &nbsp &nbsp &nbsp &nbsp"
+      boardModel._rows[i][j] = "&nbsp &nbsp &nbsp &nbsp &nbsp";
       // render boardModel's data into DOM in a pleasant way
       // fill HTML elements with blanks
-      DOMrows[i].children[j].innerHTML = blank;
+      DOMrows[i].children[j].innerHTML = boardModel._rows[i][j];
     }
   }
+
 });
+
 
 // View
 // render boardModel's data into DOM in a pleasant way
 // fill string data into <html> elements
 
-// Controller
-// add event listeners to each cell
-for (let i = 0; i < cells.length; i++) {
-  cells[i].addEventListener('click', event => {
-    // If cell is already occupied, don't allow placing
 
-    // ask to place X or O next
-    if (boardModel.placeX) {
-      cells[i].innerHTML = "&nbsp &nbsp X &nbsp &nbsp";
-    } else {
-      cells[i].innerHTML = "&nbsp &nbsp O &nbsp &nbsp";
-    }
-    // switch to X or O after placing
-    boardModel.placeX = !boardModel.placeX;
-  });
+
+// Controller
+
+// click handler should update the boardModel with appropriate piece
+// which is then reflected on DOM
+let addXorO = (cell) => {
+  if (boardModel.placeX) {
+    cell.innerHTML = "&nbsp &nbsp X &nbsp &nbsp";
+  } else {
+    cell.innerHTML = "&nbsp &nbsp O &nbsp &nbsp";
+  }
+  // switch to X or O after placing
+  boardModel.placeX = !boardModel.placeX;
+}
+
+// add event listeners to each cell
+
+
+for (let i = 0; i < DOMrows.length; i++) {
+  for (let j = 0; j < DOMrows[i].children.length; j++) {
+    let cell = DOMrows[i].children[j];
+    cell.addEventListener('click', event => {
+      addXorO(cell);
+    });
+  }
 }
 
