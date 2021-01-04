dada3d5035aa9b4fa39517c7ec1640aa50aab6f0
Justin Chow <hrll71990@gmail.com>
2021-01-04 10:58:04 -0800

    testing DOM methods to access and add listeners to each board cell (table rows)

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,20 @@
-console.log('dee em cee tu');
 
+// access all board cells directly
+// array-like format
+var cells = document.getElementsByTagName('td');
+console.log(cells);
+
+// access the entire board
+const board = document.querySelector('table');
+// HTMLTableElement methods apply to tables only
+
+// access board rows
+const rows = board.rows;
+console.log(rows);
+
+// add event listeners to each cell
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', event => {
+    console.log('event listner test');
+  })
+}
