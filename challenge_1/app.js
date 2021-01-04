be8bf98879cf9785efd0a75a8ec41a32ce3984e8
Justin Chow <hrll71990@gmail.com>
2021-01-04 15:04:27 -0800

    board model object and DOM view should now initiliaze to a blank board on page load

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -8,11 +8,11 @@ const board = document.querySelector('table');
 // HTMLTableElement methods apply to tables only
 
 // access DOM board rows
-const rows = board.rows;
+const DOMrows = board.rows;
 
 // Model - board's internal state for rows and cells, win checking
 const boardModel = {
-  rows: [[], [], []],
+  _rows: [[], [], []],
   placeX: true
   // threeInRow: 'something',
   // checkForWin: function(rows) {
@@ -21,41 +21,38 @@ const boardModel = {
 }
 // initialize board?
 document.addEventListener('DOMContentLoaded', event => {
-  boardModel.rows.forEach(row => {
-    // initialize model rows, contains no X or O, blank
-    row[0] = '1';
-    row[1] = '2';
-    row[2] = '3';
-  });
-
-  // first move always starts at player X
+  // first move always starts on player X
   boardModel.placeX = true;
 
-  for (let i = 0; i < rows.length; i++) {
-    for (let j = 0; j < rows[i].children.length; j++) {
-      rows[i].children[j].innerHTML = "&nbsp &nbsp &nbsp &nbsp &nbsp";
+  // initialize boardModel rows, contains no X or O, blank
+  for (let i = 0; i < boardModel._rows.length; i++) {
+    for (let j = 0; j < boardModel._rows[i].length; j++) {
+      let blank = "&nbsp &nbsp &nbsp &nbsp &nbsp"
+      boardModel._rows[i][j] = blank;
+      // render boardModel's data into DOM in a pleasant way
+      // fill HTML elements with blanks
+      DOMrows[i].children[j].innerHTML = blank;
     }
   }
 });
-
-// boardModel.rows.forEach(row => {
-//   console.log(row);
-// })
 
 // View
 // render boardModel's data into DOM in a pleasant way
 // fill string data into <html> elements
 
-
 // Controller
 // add event listeners to each cell
 for (let i = 0; i < cells.length; i++) {
   cells[i].addEventListener('click', event => {
+    // If cell is already occupied, don't allow placing
+
+    // ask to place X or O next
     if (boardModel.placeX) {
       cells[i].innerHTML = "&nbsp &nbsp X &nbsp &nbsp";
     } else {
       cells[i].innerHTML = "&nbsp &nbsp O &nbsp &nbsp";
     }
+    // switch to X or O after placing
     boardModel.placeX = !boardModel.placeX;
   });
 }
