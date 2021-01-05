85aadded990596d096e05ca6187b06386077ba9e
Justin Chow <hrll71990@gmail.com>
2021-01-05 11:22:39 -0800

    can get form data from textarea on button click, nodemon installed

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,4 +1,13 @@
 // Express to serve up index.html and assets
 console.log('Hello world');
+
 // Client should be able to submit JSON data to server, receive response of CSV-format
 
+// obtain user input from form element
+var userData = document.getElementById('data');
+const button = document.querySelector('input');
+
+button.addEventListener('click', event => {
+  console.log(userData.value);
+  event.preventDefault();
+})
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -4,16 +4,25 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('client'));
+app.use('/', (req, res, next) => {
+  console.log(`Incoming ${req.method} from ${req.path}`);
+  next();
+});
+
 
 app.get('/', (req, res) => {
   res.send('Testing mini app 2 server.');
-})
+});
+
+app.post('/', (req, res) => {
+  res.send(`${req.body} received. POST Request`);
+});
 
 // Client should be able to submit JSON data to server, receive response of CSV-format
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
-})
+});
 
 /*
 The server must flatten the JSON hierarchy, mapping each item/object in the JSON to a single line of CSV report (see included sample output), where the keys of the JSON objects will be the columns of the CSV report.
