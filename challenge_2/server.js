8c7362167dee5f6ccb20cd54b6de2c72aee34bcb
Justin Chow <hrll71990@gmail.com>
2021-01-06 03:52:26 -0800

    textarea replaced with file input, server.js now uses given file to find filepath to json, reads json content to convert to cs

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,7 +1,8 @@
-// Express to serve up index.html and assets
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = 4000;
 const url = '/csv_generator';
 
 app.use(express.static('client'));
@@ -13,22 +14,14 @@ app.use('/', (req, res, next) => {
   next();
 });
 
-app.get(url, (req, res) => {
-  res.send('Testing mini app 2 server.');
-});
 
-app.post(url, (req, res) => {
-  // convert req.body to csv format, then send it back
-  var rawJSON = req.body.jsonData;
-  var parsedJSON = JSON.parse(rawJSON);
-
-  // all initial keys except "children" are columns for csv
+function jsonToCSV(json) {
   var result = [];
-  let columnHeaders = Object.keys(parsedJSON);
+  let columnHeaders = Object.keys(json);
   columnHeaders.pop();
   result = columnHeaders.join(',') + '\n';
 
-  function jsonToCSV(obj) {
+  function converter(obj) {
     let line = [];
     for (key in obj) {
       if (key !== 'children') {
@@ -40,50 +33,66 @@ app.post(url, (req, res) => {
       } else {
         let children = obj[key]
         for (let i = 0; i < children.length; i++) {
-          jsonToCSV(children[i]);
+          converter(children[i]);
         }
       }
     }
   }
-  jsonToCSV(parsedJSON);
+  converter(json)
+  return template(result);
+}
 
-  // send back html template of page, but with csv also attached
-  // this is very gross but works for now?
-  function template(responseData) {
-    return `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Challenge 2: CSV Report Generator</title>
-        <link rel="stylesheet" href="styles.css">
-      </head>
-      <body>
-        <h1>CSV Report Generator</h1>
-        <form method="POST" action="/csv_generator">
-          <textarea id="data" name="jsonData"></textarea>
-          <input type="submit" value="Submit">
-        </form>
-        <div classname="server-response">
-          ${responseData}
-        <div>
-      </body>
-      <script src="app.js"></script>
-    </html>
-    `
-  }
+function template(responseData) {
+  return `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Challenge 2: CSV Report Generator</title>
+      <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+      <h1>CSV Report Generator</h1>
+      <form method="POST" action="/csv_generator">
+        <textarea id="data" name="jsonData"></textarea>
+        <input type="submit" value="Submit">
+      </form>
+      <div classname="server-response">
+        ${responseData}
+      <div>
+    </body>
+    <script src="app.js"></script>
+  </html>
+  `
+}
 
-  res.send(template(result));
+const readJSON = (filepath, callback) => {
+  fs.readFile(filepath, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, data);
+    }
+  });
+}
+
+app.post(url, (req, res) => {
+  // convert req.body to csv format, then send it back
+  var filepath = path.resolve(__dirname, 'samples', req.body.jsonData);
+
+  readJSON(filepath, (err, rawJSON) => {
+    if (err) {
+      console.log('read file error', err);
+    } else {
+      var parsedJSON = JSON.parse(rawJSON)
+      var csv = jsonToCSV(parsedJSON);
+      res.send(csv);
+    }
+  })
+
+
 });
-
-// Client should be able to submit JSON data to server, receive response of CSV-format
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
 });
-
-/*
-The server must flatten the JSON hierarchy, mapping each item/object in the JSON to a single line of CSV report (see included sample output), where the keys of the JSON objects will be the columns of the CSV report.
-You may assume the JSON data has a regular structure and hierarchy (see included sample file). In other words, all sibling records at a particular level of the hierarchy will have the same set of properties, but child objects might not contain the same properties. In all cases, every property you encounter must be present in the final CSV output.
-You may also assume that child records in the JSON will always be in a property called `children`.
-*/
