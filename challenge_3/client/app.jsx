685b306321fc07d70709af5f1029599f9db4ccc4
Justin Chow <hrll71990@gmail.com>
2021-01-06 11:01:31 -0800

    environment setup fix

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,23 @@
-console.log('hello worldsss')
+// Use React to build front end
 
+// AT SOME POINT: make MySQL db to store user data
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        React is working
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,15 @@
+// Express to serve index.html + assets
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  console.log(`Incoming ${req.method} request from url ${req.url}`);
+  next();
+})
+
+app.listen(port, ()=> {
+  console.log(`Express listening at port ${port}!`);
+})
