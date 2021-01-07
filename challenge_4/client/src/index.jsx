eed7d6a942d097deb4fa6a55938b97dfc07dc045
Justin Chow <hrll71990@gmail.com>
2021-01-07 10:11:06 -0800

    challenge 4 environment setup

---
+++ challenge_4/client/src/index.jsx
@@ -0,0 +1,8 @@
+// import all necessary components from ./components
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app.jsx';
+
+
+// render everything to DOM
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.static('./client/dist'));
+
+app.use((req, res, next) => {
+  console.log(`Incoming ${req.method} request from ${req.path}`);
+  next();
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Express listening on port ${port}!`);
+});
 
